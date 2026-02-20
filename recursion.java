df55319c0e6a9b8d0e9a79940d3f3882a29a34ee
Ado <adityaq05@gmail.com>
2026-02-20 14:44:37 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,11 @@
+public class recursion {
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+    public static void main(String[] args) {
+        System.err.println("Sum of digits: " + sumOfDigits(1234));
+    }
+}
