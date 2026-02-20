6c64fdfc8ed408d355c92cf0c9d78b6e06ebbc3d
Ado <adityaq05@gmail.com>
2026-02-20 14:51:04 +0530

    Add methods to count and reverse digits in recursion

--- recursion.java
+++ recursion.java
@@ -5,7 +5,22 @@ public class recursion {
         }
         return (n % 10) + sumOfDigits(n / 10);
     }
+    static int countOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return 1 + countOfDigits(n / 10);
+    }
+    static int reverse(int n, int rev) {
+        if (n == 0) {
+            return rev;
+        }
+        rev = rev * 10 + n % 10;
+        return reverse(n / 10, rev);
+    }
     public static void main(String[] args) {
         System.err.println("Sum of digits: " + sumOfDigits(1234));
+        System.err.println("Count of digits: " + countOfDigits(1234));
+        System.err.println("Reverse of digits: " + reverse(1234, 0));
     }
 }
