3f8fbaa2d1e6f603c2033f7efd643fe220838b97
dai-ski <adityaq0p@gmail.com>
2026-05-08 07:37:41 +0530

    Update K+W.py

--- K+W.py
+++ K+W.py
@@ -1,10 +1,6 @@
 from gensim.downloader import load
 model = load("glove-wiki-gigaword-50")
-
-# king - man + woman = queen
-res = model.most_similar(positive=['king', 'woman'], negative=['man'], topn=1)
-print(res[0][0]) 
-
-# top 5 similar words
-res5 = model.most_similar(positive=['programming'], topn=5)
-print(res5)
+# Analogy: king - man + woman = queen
+print(model.most_similar(positive=['king', 'woman'], negative=['man'], topn=1))
+# Top 5 similar words
+print(model.most_similar(positive=['programming'], topn=5))
