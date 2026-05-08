cea7db53b8b2a13a8d74b18ea76afbdc7a5872f4
dai-ski <adityaq0p@gmail.com>
2026-05-08 09:34:26 +0530

    Trim PCA_WordEmbeddings.py

--- PCA_WordEmbeddings.py
+++ PCA_WordEmbeddings.py
@@ -1,21 +1,10 @@
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from gensim.downloader import load
-
-print("Loading GloVe model...")
 model = load("glove-wiki-gigaword-50")
-
 words = ['football', 'basketball', 'soccer', 'tennis', 'cricket']
-embeddings = [model[word] for word in words]
-
-reduced = PCA(n_components=2).fit_transform(embeddings)
-
-plt.figure(figsize=(8, 6))
-for i, word in enumerate(words):
-    plt.scatter(reduced[i, 0], reduced[i, 1])
-    plt.text(reduced[i, 0] + 0.02, reduced[i, 1] + 0.02, word)
-
-plt.title("PCA Visualization of Word Embeddings")
-plt.grid(True)
+vecs = PCA(2).fit_transform([model[w] for w in words])
+for i, w in enumerate(words):
+    plt.scatter(vecs[i,0], vecs[i,1])
+    plt.text(vecs[i,0], vecs[i,1], w)
 plt.show()
-
