307203db8eac4cb271cce183adaed7edc763a5a7
dai-ski <adityaq0p@gmail.com>
2026-05-08 07:37:33 +0530

    Rename prog3.py to Word2Vec_Similarity.py

---
+++ Word2Vec_Similarity.py
@@ -0,0 +1,13 @@
+from gensim.models import Word2Vec
+
+corpus = ["The patient was diagnosed with diabetes.", "Symptoms include fever."]
+tokenized = [s.lower().split() for s in corpus]
+
+# Train Model
+model = Word2Vec(sentences=tokenized, vector_size=5, window=2, min_count=1, epochs=5)
+
+# Find similar
+word = input("Enter word: ").lower()
+if word in model.wv:
+    print(model.wv.most_similar(word, topn=5))
+    
